e5c40aeec6a55e134f172a4cc4a3587c8340284e
Julio Souza <soneka1234@hotmail.com>
2021-07-07 08:04:25 -0300

    Criando mongoDBContext

--- src/Avanade-SubTCSE-Projeto.Data/Repositories/Base/MongoDB/MongoDBContext.cs
+++ src/Avanade-SubTCSE-Projeto.Data/Repositories/Base/MongoDB/MongoDBContext.cs
@@ -5,6 +5,18 @@ namespace Avanade_SubTCSE_Projeto.Data.Repositories.Base.MongoDB
 {
     public class MongoDBContext : IMongoDBContext
     {
+        public MongoDBContext()
+        {
+            MongoClientSettings mongoClientSettings = MongoClientSettings.FromUrl(new MongoUrl(""));
+
+            mongoClientSettings.SslSettings = new SslSettings()
+            {
+                EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12
+            };
+
+            var mongoClient = new MongoClient(settings: mongoClientSettings);
+        }
+
         public IMongoCollection<TEntity> GetCollection<TEntity>(string collection)
         {
             throw new System.NotImplementedException();
